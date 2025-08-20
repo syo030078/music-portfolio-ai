d63c6d780f5e9fdf8b20ff3cc3fa8e8887137c12
Syo Miyata <44368100+syo030078@users.noreply.github.com>
2025-08-20 11:30:49 +0900

    JwtDenylistモデルの追加とDevise JWT設定の修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_08_20_020719) do
+ActiveRecord::Schema[7.0].define(version: 2025_08_20_022943) do
+  create_table "jwt_denylists", force: :cascade do |t|
+    t.string "jti"
+    t.datetime "exp"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["jti"], name: "index_jwt_denylists_on_jti"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
