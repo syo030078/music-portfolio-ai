c4384bfbc0a9d86a9c99893b73d5154e0d6e1ce5
Syo Miyata <44368100+syo030078@users.noreply.github.com>
2025-08-21 18:47:04 +0900

    feat(user): add name column to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_08_20_022943) do
+ActiveRecord::Schema[7.0].define(version: 2025_08_20_030443) do
   create_table "jwt_denylists", force: :cascade do |t|
     t.string "jti"
     t.datetime "exp"
@@ -27,8 +27,8 @@ ActiveRecord::Schema[7.0].define(version: 2025_08_20_022943) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
 end
