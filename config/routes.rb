74a50041a82dc7fb20528eadb70e9c60bb503125
Syo Miyata <44368100+syo030078@users.noreply.github.com>
2025-08-21 18:45:40 +0900

    chore(auth): enforce authentication in ApplicationController

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: 'auth', controllers: {
+  sessions: 'auth/sessions'
+}
+
+  namespace :api do
+    namespace :v1 do
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
