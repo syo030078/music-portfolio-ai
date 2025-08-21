efb5f3f486e3eddfd388aeb33e8e25e56804d6f3
Syo Miyata <44368100+syo030078@users.noreply.github.com>
2025-08-21 18:36:02 +0900

    feat(auth): enable Devise modules on User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
+# app/models/user.rb
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
+  has_many :tracks, dependent: :destroy
 end
 
 
