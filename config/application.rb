05c780efc0c615f763fd205d6affd91a65c82335
Syo Miyata <44368100+syo030078@users.noreply.github.com>
2025-08-28 10:30:00 +0900

    chore(config): improve application configuration with timezone and CORS

--- config/application.rb
+++ config/application.rb
@@ -18,12 +18,20 @@ module MusicPortfolioAi
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Asia/Tokyo"
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Configure CORS
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      end
+    end
   end
 end
