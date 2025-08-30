ccc3ebbf59f2760e85132ccf67d10d985a399603
Syo Miyata <44368100+syo030078@users.noreply.github.com>
2025-08-30 17:15:19 +0900

    feat(users): add UsersController following Rails conventions
    - Replace ProfilesController with Rails-compliant UsersController
    - Fix routing to use proper resource :user mapping
    - Improve code maintainability and consistency
    - Follow RESTful API design principles

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,23 @@
+class Api::V1::UsersController < ApplicationController
+  def show
+    render json: {
+      id: current_user.id,
+      email: current_user.email,
+      name: current_user.name
+    }
+  end
+
+  def update
+    if current_user.update(user_params)
+      render json: current_user
+    else
+      render json: { errors: current_user.errors }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name)
+  end
+end
