5d2f0e08f6b761f22ccba509107425faafc9117a
Syo Miyata <44368100+syo030078@users.noreply.github.com>
2025-08-30 17:04:52 +0900

    Revert "feat(profiles): add ProfilesController"
    This reverts commit 80fd4d2b7acc089769d3e90ecf4dc80fb428c9dd.

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resource :profile, only: [:show, :update]
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
